36b50c40669b2abbb1516e58f1f68505a39f6f53
liuyi <liuyi@hetao101.com>
2020-06-12 00:47:30 +0800

    添加 tools 包

--- src/util/thirdParty.js
+++ src/util/thirdParty.js
@@ -1,7 +1,19 @@
+import Moment from 'moment'
 import Drag from '@stroll/drag'
+import Tools from '@stroll/tools'
+
+export function getTime (time, format) {
+  return time ? Moment(time).format(format) : '--'
+}
+export const getDataTime = (time) => {
+  return time ? Moment(time).valueOf() : '--'
+}
 
 export default {
   install: Vue => {
     Vue.use(Drag)
+    Vue.prototype.$tools = Tools
+    Vue.prototype.$getTime = getTime
+    Vue.prototype.$getDataTime = getDataTime
   }
 }
